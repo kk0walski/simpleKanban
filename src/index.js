512aaa9578fa6924e8fadf33c787aee777a23c29
kk0walski <karol.kowalski95@gmail.com>
2018-09-10 17:51:56 +0200

    Prosta lista z wykorzystaniem beautiful-dnd.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './Column';
+export default class App extends Component {
+    state = initialData;
 
-const App = () => 'Hello world';
+    onDragEnd = result => {
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+    }
+
+    render() {
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                {this.state.columnOrder.map((columnId) => {
+                    const column = this.state.columns[columnId];
+                    const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+                    return <Column key={column.id} column={column} tasks={tasks} />;
+                })}
+            </DragDropContext>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
