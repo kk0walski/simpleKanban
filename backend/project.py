52bdf86cd5cf9fafe93e27b47785a2f0f7700e98
kk0walski <karol.kowalski95@gmail.com>
2018-09-13 01:21:52 +0200

    Konfiguracja backendu.

--- backend/project.py
+++ backend/project.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from setup_database import Board, Base, List, Card
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///board.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def hello_world():
---
+++ backend/setup_database.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import json
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Board(Base):
+    __tablename__ = 'Board'
+
+    index = Column(Integer, primary_key=True)
+    lists = Column(String, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'lists': json.loads(self.lists)
+        }
+
+class List(Base):
+    __tablename__ = "List"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    cards = Column(String, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'cards': json.loads(self.lists)
+        }
+
+class Card(Base):
+    __tablename__ = "Card"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content
+        }
+
+engine = create_engine('sqlite:///board.db')
+
+Base.metadata.create_all(engine)
