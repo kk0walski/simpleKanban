6478214e8145a6fd63a7da56c6be55bc1c41aec4
kk0walski <karol.kowalski95@gmail.com>
2018-09-14 00:14:38 +0200

    Adding redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware compose } from 'redux';
 import Board from '../reducers/Board';
 import Cards from '../reducers/Cards';
 import Lists from '../reducers/Lists';
+import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -12,6 +13,6 @@ export default () => {
         Cards,
         Lists
     })
-    const store = createStore(appReducer, composeEnhancers);
+    const store = createStore(appReducer, composeEnhancers(applyMiddleware(thunk)));
     return store
 }
