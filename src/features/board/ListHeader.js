3c88f5ad5e9fafd72f1a4d5694ca4d30cdfd9cb3
kk0walski <karol.kowalski95@gmail.com>
2021-09-15 16:40:03 +0200

    Simpler listTitle.

--- src/features/board/ListHeader.js
+++ src/features/board/ListHeader.js
@@ -1,111 +1,19 @@
 
 import React, { Component } from "react";
-import Textarea from "react-textarea-autosize";
-import { Button, Wrapper, Menu, MenuItem } from "react-aria-menubutton";
-import { FaTrash } from "react-icons/fa";
 import "./ListHeader.scss";
 
 export default class ListTitle extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-      newTitle: props.listTitle
-    };
-  }
-
-  handleChange = event => {
-    this.setState({ newTitle: event.target.value });
-  };
-
-  handleKeyDown = event => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      this.handleSubmit();
-    } else if (event.keyCode === 27) {
-      this.revertTitle();
-    }
-  };
-
-  handleSubmit = () => {
-    const { newTitle } = this.state;
-    const { listTitle, listId, dispatch } = this.props;
-    if (newTitle === "") return;
-    // if (newTitle !== listTitle) {
-    //   dispatch({
-    //     type: "CHANGE_LIST_TITLE",
-    //     payload: { listTitle: newTitle, listId }
-    //   });
-    // }
-    this.setState({ isOpen: false });
-  };
-
-  revertTitle = () => {
-    this.setState({ newTitle: this.props.listTitle, isOpen: false });
-  };
-
-  deleteList = () => {
-    const { listId, cards, boardId, dispatch } = this.props;
-    // dispatch({
-    //   type: "DELETE_LIST",
-    //   payload: { cards, listId, boardId }
-    // });
-  };
-
-  openTitleEditor = () => {
-    this.setState({ isOpen: true });
-  };
-
-  handleButtonKeyDown = event => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      this.openTitleEditor();
-    }
-  };
-
   render() {
-    const { isOpen, newTitle } = this.state;
-    const { dragHandleProps, listTitle } = this.props;
     return (
       <div className="list-header">
-        {isOpen ? (
-          <div className="list-title-textarea-wrapper">
-            <Textarea
-              autoFocus
-              useCacheForDOMMeasurements
-              value={newTitle}
-              onChange={this.handleChange}
-              onKeyDown={this.handleKeyDown}
-              className="list-title-textarea"
-              onBlur={this.handleSubmit}
-              spellCheck={false}
-            />
-          </div>
-        ) : (
-          <div
-            {...dragHandleProps}
+        <div
+            {...this.props.dragHandleProps}
             role="button"
             tabIndex={0}
-            onClick={this.openTitleEditor}
-            onKeyDown={event => {
-              this.handleButtonKeyDown(event);
-              dragHandleProps.onKeyDown(event);
-            }}
             className="list-title-button"
           >
-            {listTitle}
+            {this.props.listTitle}
           </div>
-        )}
-        <Wrapper className="delete-list-wrapper" onSelection={this.deleteList}>
-          <Button className="delete-list-button">
-            <FaTrash />
-          </Button>
-          <Menu className="delete-list-menu">
-            <div className="delete-list-header">Are you sure?</div>
-            <MenuItem className="delete-list-confirm">Delete</MenuItem>
-          </Menu>
-        </Wrapper>
       </div>
     );
   }
